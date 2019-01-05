dd5b956e41f936e66bc8975e3ab712ac1c7e012c
HeyyD <heidi.hati@cs.tamk.fi>
2019-01-05 23:10:55 +0200

    fetch album data

--- src/app/components/photo-page/photo-page.component.ts
+++ src/app/components/photo-page/photo-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PhotoService } from 'src/app/services/photo-service/photo.service';
 import { ActivatedRoute } from '@angular/router';
 import { Photo } from 'src/app/interfaces/photo';
+import { AlbumService } from 'src/app/services/album-service/album.service';
 
 @Component({
   selector: 'app-photo-page',
@@ -12,12 +13,21 @@ export class PhotoPageComponent implements OnInit {
 
   photo: Photo;
 
-  constructor(private route: ActivatedRoute, private photoService: PhotoService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private photoService: PhotoService,
+    private albumService: AlbumService
+  ) { }
 
   ngOnInit(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.photoService.getPhoto(id).subscribe(res => {
       this.photo = res;
+      this.initAlbum();
     });
+  }
+
+  private initAlbum(): void {
+    this.albumService.getAlbum(this.photo.albumId).subscribe(res => console.log(res));
   }
 }
