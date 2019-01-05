d0633ecfbe38a3cfc23b4815edd4cd95c9e5ea7f
HeyyD <heidi.hati@cs.tamk.fi>
2019-01-05 21:17:18 +0200

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 import { AppComponent } from './app.component';
 import { PhotoListComponent } from './components/photo-list/photo-list.component';
+
+const appRoutes: Routes = [
+  { path: 'photos', component: PhotoListComponent },
+  { path: '**', redirectTo: '/photos', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
@@ -13,6 +19,7 @@ import { PhotoListComponent } from './components/photo-list/photo-list.component
     PhotoListComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     HttpClientModule,
     InfiniteScrollModule
