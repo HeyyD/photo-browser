f19bbfa6fa64f0bbe302592e58b4e8dd17a3bbaa
HeyyD <heidi.hati@cs.tamk.fi>
2019-01-05 17:59:22 +0200

    new component photo list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PhotoListComponent } from './components/photo-list/photo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoListComponent
   ],
   imports: [
     BrowserModule
