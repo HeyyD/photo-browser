17770cf539edd94dd3da9e18ac201cc079777486
HeyyD <heidi.hati@cs.tamk.fi>
2019-01-06 15:02:49 +0200

    reload after album picture change

--- src/app/components/photo-page/photo-page.component.ts
+++ src/app/components/photo-page/photo-page.component.ts
@@ -1,60 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PhotoService } from 'src/app/services/photo-service/photo.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { Photo } from 'src/app/interfaces/photo';
 import { AlbumService } from 'src/app/services/album-service/album.service';
 import { Album } from 'src/app/interfaces/album';
 import { UserService } from 'src/app/services/user-service/user.service';
 import { User } from 'src/app/interfaces/user';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-photo-page',
   templateUrl: './photo-page.component.html',
   styleUrls: ['./photo-page.component.scss']
 })
-export class PhotoPageComponent implements OnInit {
+export class PhotoPageComponent implements OnDestroy {
 
   photo: Photo;
   album: Album;
   user: User;
   albumPhotos: Photo[];
 
+  subscriptios: Subscription[] = [];
+
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private photoService: PhotoService,
     private albumService: AlbumService,
     private userService: UserService
-  ) { }
-
-  ngOnInit(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.photoService.getPhoto(id).subscribe(res => {
-      this.photo = res;
-      this.initAlbumPhotos();
-      this.initAlbum();
+  ) {
+    const sub = this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.init();
+      }
     });
+
+    this.subscriptios.push(sub);
+  }
+
+  ngOnDestroy() {
+    this.subscriptios.forEach(subscription => subscription.unsubscribe());
   }
 
   isReady(): boolean {
     return !!(this.photo && this.album && this.user && this.albumPhotos);
   }
 
+  private init() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    const sub = this.photoService.getPhoto(id).subscribe(res => {
+      this.photo = res;
+      this.initAlbumPhotos();
+      this.initAlbum();
+    });
+
+    this.subscriptios.push(sub);
+  }
+
   private initAlbum(): void {
-    this.albumService.getAlbum(this.photo.albumId).subscribe(res => {
+    const sub = this.albumService.getAlbum(this.photo.albumId).subscribe(res => {
       this.album = res;
       this.initUser();
     });
+
+    this.subscriptios.push(sub);
   }
 
   private initUser(): void {
-    this.userService.getUser(this.album.userId).subscribe(res => {
+    const sub = this.userService.getUser(this.album.userId).subscribe(res => {
       this.user = res;
     });
+
+    this.subscriptios.push(sub);
   }
 
   private initAlbumPhotos(): void {
-    this.photoService.getAlbumPhotos(this.photo.albumId).subscribe(res => {
+    const sub = this.photoService.getAlbumPhotos(this.photo.albumId).subscribe(res => {
       this.albumPhotos = res;
     });
+
+    this.subscriptios.push(sub);
   }
 }
