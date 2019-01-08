64acaa9ac0691a80f6a27e57353a2ed645a6fcfe
HeyyD <heidi.hati@cs.tamk.fi>
2019-01-08 21:14:18 +0200

    list users albums

--- src/app/components/album/album.component.ts
+++ src/app/components/album/album.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Album } from 'src/app/interfaces/album';
+import { Photo } from 'src/app/interfaces/photo';
+import { PhotoService } from 'src/app/services/photo-service/photo.service';
 
 @Component({
   selector: 'app-album',
@@ -10,9 +12,12 @@ export class AlbumComponent implements OnInit {
 
   @Input() album: Album;
 
-  constructor() { }
+  photos: Photo[];
+
+  constructor(private photoService: PhotoService) { }
 
   ngOnInit() {
+    this.photoService.getAlbumPhotos(this.album.id).subscribe(res => this.photos = res.slice(0, 5));
   }
 
 }
