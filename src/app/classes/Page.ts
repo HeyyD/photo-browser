61167d0e4ff6e6d9b4d36bd00dbcd771ed5613f0
HeyyD <heidi.hati@cs.tamk.fi>
2019-01-17 11:27:42 +0200

    add destroy and add subscriptions

--- src/app/classes/Page.ts
+++ src/app/classes/Page.ts
@@ -1,5 +1,14 @@
 import { Subscription } from 'rxjs';
+import { OnDestroy } from '@angular/core';
 
-abstract class Page {
+abstract class Page implements OnDestroy {
   private subscriptions: Subscription[] = [];
+
+  protected addSubscription(subscription: Subscription) {
+    this.subscriptions.push(subscription);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
 }
