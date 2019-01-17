74ca8f47a3c91cdaf1848b47fd7583d5587ef9a0
HeyyD <heidi.hati@cs.tamk.fi>
2019-01-17 12:22:34 +0200

    new method addSubscriptions

--- src/app/classes/Page.ts
+++ src/app/classes/Page.ts
@@ -4,8 +4,12 @@ import { OnDestroy } from '@angular/core';
 export default abstract class Page implements OnDestroy {
   private subscriptions: Subscription[] = [];
 
-  protected addSubscription(subscription: Subscription) {
+  protected addSubscription(subscription: Subscription): void {
     this.subscriptions.push(subscription);
+  }
+
+  protected addSubscriptions(subscriptions: Subscription[]): void {
+    subscriptions.forEach(subscription => this.subscriptions.push(subscription));
   }
 
   ngOnDestroy(): void {
